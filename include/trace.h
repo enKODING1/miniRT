eef2078ebd168947831053d5c6075587bd4576e7
enkoding1 <ksjdevelop53@gmail.com>
2025-12-12 18:41:27 +0900

    feat: 퐁라이트 적용

--- include/trace.h
+++ include/trace.h
@@ -14,4 +14,5 @@ t_bool hit_sphere(t_object *obj, t_ray *ray, t_hit_record *rec);
 void set_face_normal(t_ray *r, t_hit_record *rec);
 t_color3 phong_lighting(t_scene *scene);
 
+t_color3 point_light_get(t_scene *scene, t_light *light);
 #endif
--- main.c
+++ main.c
@@ -19,9 +19,9 @@ t_scene *scene_init(void)
     scene->ambient = vmult(color3(1, 1, 1), ka); // 8.4 에서 설명
     scene->canvas = canvas(400, 300);
     scene->camera = camera(&scene->canvas, point3(0, 0, 0));
-    world = object(SP, sphere(point3(-2, 0, -5), 2), color3(0.5, 0, 0));         // world 에 구1 추가
-    oadd(&world, object(SP, sphere(point3(2, 0, -5), 2), color3(0, 0.5, 0)));    // world 에 구2 추가
-    oadd(&world, object(SP, sphere(point3(0, -1000, 0), 999), color3(1, 1, 1))); // world 에 구3 추가
+    world = object(SP, sphere(point3(-2, 0, -5), 2), color3(0.5, 0, 0));          // world 에 구1 추가
+    oadd(&world, object(SP, sphere(point3(2, 0, -5), 2), color3(0, 0.5, 0)));     // world 에 구2 추가
+    oadd(&world, object(SP, sphere(point3(0, -1000, 0), 1000), color3(1, 1, 1))); // world 에 구3 추가
     scene->world = world;
     lights = object(LIGHT_POINT, light_point(point3(0, 5, 0), color3(1, 1, 1), 0.5), color3(0, 0, 0)); // 더미 albedo
     scene->light = lights;
